b675bbe9aa97217ab8c2562fa9b7184f06a6ac6c
Jacob Husband <jakehusband2@gmail.com>
2022-10-27 14:16:28 -0700

    Add map to screen on button click

--- dist/main.js
+++ dist/main.js
@@ -73,13 +73,48 @@ __webpack_require__.r(__webpack_exports__);
 class Map extends (react__WEBPACK_IMPORTED_MODULE_0___default().Component) {
   constructor(props) {
     super(props);
-    this.state = {
-      showingMap: false
-    };
+    this.showMap = this.showMap.bind(this);
+    this.map = null;
+  }
+  showMap(event) {
+    console.log(this.props);
+    this.map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: 33.634929,
+        lng: -117.7405074
+      },
+      zoom: 18,
+      minZoom: 15,
+      maxZoom: 21
+    });
+    const drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.OverlayType.POLYLINE]
+      },
+      polylineOptions: {
+        editable: true,
+        clickable: true
+      },
+      circleOptions: {
+        fillColor: "#ffff00",
+        fillOpacity: 1,
+        strokeWeight: 5,
+        clickable: false,
+        editable: true,
+        zIndex: 1
+      }
+    });
+    drawingManager.setMap(this.map);
   }
   render() {
     return /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default().createElement("button", {
-      onClick: this.props.handleShowMapClick
+      onClick: event => {
+        this.props.handleShowMapClick;
+        this.showMap;
+      }
     }, "Open Map");
   }
 }
--- src/map.jsx
+++ src/map.jsx
@@ -3,14 +3,44 @@ import React from 'react'
 export default class Map extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {
-      showingMap: false
-    }
+    this.showMap = this.showMap.bind(this)
+    this.map = null;
+  }
+
+  showMap(event) {
+    console.log(this.props)
+    this.map = new google.maps.Map(document.getElementById("map"), {
+      center: { lat: 33.634929, lng: -117.7405074 },
+      zoom: 18,
+      minZoom: 15,
+      maxZoom: 21,
+    });
+    const drawingManager = new google.maps.drawing.DrawingManager({
+      drawingMode: google.maps.drawing.OverlayType.MARKER,
+      drawingControl: true,
+      drawingControlOptions: {
+        position: google.maps.ControlPosition.TOP_CENTER,
+        drawingModes: [google.maps.drawing.OverlayType.POLYLINE],
+      },
+      polylineOptions: {
+        editable: true,
+        clickable: true,
+      },
+      circleOptions: {
+        fillColor: "#ffff00",
+        fillOpacity: 1,
+        strokeWeight: 5,
+        clickable: false,
+        editable: true,
+        zIndex: 1,
+      },
+    });
+    drawingManager.setMap(this.map);
   }
 
   render() {
     return (
-      <button onClick={this.props.handleShowMapClick}>Open Map</button>
+      <button onClick={(event) => {this.props.handleShowMapClick; this.showMap}}>Open Map</button>
     )
   }
 }
