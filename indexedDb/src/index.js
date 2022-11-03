2d0e7e5100c4c3f622a0f51d3f96af8ca699da03
Jacob Husband <jakehusband2@gmail.com>
2022-11-02 18:40:50 -0700

    Add cacheing and a service worker to handle offline mode

--- indexedDb/src/index.js
+++ indexedDb/src/index.js
@@ -1,15 +1,40 @@
 import { set, get, update } from "idb-keyval";
 
 const latlng = [];
+let count = 0;
+let id;
+
+const registerServiceWorker = async () => {
+  if ("serviceWorker" in window.navigator) {
+    try {
+      const registration = await window.navigator.serviceWorker.register("sw.js");
+      if (registration.installing) {
+        console.log("Service worker installing!");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    } catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  }
+};
+
+registerServiceWorker();
 
 set("latlng", latlng)
   .then(() => {
     console.log("Wiped array");
-    setInterval(findPosition, 3000);
+    id = setInterval(findPosition, 3000);
   })
   .catch((err) => console.error(err));
 
 function findPosition() {
+  count++;
+  if (count === 5) {
+    clearInterval(id);
+  }
   window.navigator.geolocation.getCurrentPosition((position) => {
     const time = new Date().getTime() / 1000;
     const newPos = {
