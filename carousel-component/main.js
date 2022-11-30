731f26c2075c201c305a10b994ea583a01d58187
Jacob Husband <jakehusband2@gmail.com>
2022-11-30 00:03:52 -0800

    Turn carousel into class component

--- carousel-component/main.js
+++ carousel-component/main.js
@@ -1,7 +1,4 @@
 const body = document.body;
-const overlay = document.querySelector('.overlay');
-const dots = overlay.querySelector('.dots');
-let id = 1;
 
 const urls = [
   'https://www.livingspaces.com/globalassets/productassets/200000-299999/250000-259999/253000-253999/253100-253199/253180/253180_grey_fabric_reversible_sofa_chaise_signature_01.jpg?w=820&h=553&mode=pad',
@@ -9,50 +6,85 @@ const urls = [
   'https://www.livingspaces.com/globalassets/productassets/200000-299999/250000-259999/253000-253999/253100-253199/253180/253180_grey_fabric_reversible_sofa_chaise_side_18.jpg?w=820&h=553&mode=pad'
 ]
 
-overlay.addEventListener('click', handleOverlayClicks);
+class Carousel {
+  constructor(urls) {
+    this.images = urls;
+    this.id = 0;
+    this.$carousel = this.buildCarousel();
+    this.$overlay = this.$carousel.querySelector('.overlay');
+    this.$dots = this.$overlay.querySelector('.dots');
+    this.handleOverlayClicks = this.handleOverlayClicks.bind(this);
+    this.rotateImage = this.rotateImage.bind(this);
+    this.$overlay.addEventListener('click', this.handleOverlayClicks);
+  }
 
-function handleOverlayClicks(event) {
-  document.querySelector(`img[data-id="${id}"]`).classList.add('hidden')
-  if (event.target.classList.value.includes('left')) rotateImage('left')
-  else if (event.target.classList.value.includes('right')) rotateImage('right')
-  else if (event.target.classList.value.includes('dot')) id = Number(event.target.dataset.id)
-  document.querySelector(`img[data-id="${id}"]`).classList.remove('hidden')
-  Array.from(dots.children).forEach(dot => {
-    (Number(dot.dataset.id) === id)
-      ? dot.firstElementChild.classList.add('filled')
-      : dot.firstElementChild.classList.remove('filled')
-  })
-}
+  buildElement(tag, attr, children) {
+    const el = document.createElement(tag);
+    for (var key in attr) (key === 'textContent')
+      ? el.textContent = attr[key]
+      : key.includes('dataset')
+        ? el.dataset[key.split('-')[1]] = attr[key]
+        : el.setAttribute(key, attr[key]);
+    if (children) children.forEach(child => (child) && el.appendChild(child));
+    return el;
+  }
 
-function rotateImage(direction) {
-  if (direction === 'right') {
-    if (id === urls.length) {
-      id = 1;
+  buildCarousel(images) {
+    const dots = [];
+    let dotClassName, imageClassName;
+
+    this.images = this.images.map(url => {
+      this.id++;
+      dotClassName = (this.id === 1) ? 'dot filled' : 'dot';
+      imageClassName = (this.id === 1) ? '' : 'hidden';
+      dots.push(this.buildElement('button', { class: 'dot', 'dataset-id': this.id }, [
+        this.buildElement('div', { class: dotClassName, 'dataset-id': this.id })
+      ]));
+      return this.buildElement('img', { src: url, class: imageClassName, 'dataset-id': this.id });
+    })
+
+    this.id = 1;
+
+    return this.buildElement('div', { class: 'carousel-container' }, [
+      ...this.images,
+      this.buildElement('div', { class: 'overlay' }, [
+        this.buildElement('button', { class: 'left-chevron' }, [
+          this.buildElement('i', { class: 'fas fa-chevron-circle-left' })
+        ]),
+        this.buildElement('div', { class: 'dots' }, [
+          ...dots
+        ]),
+        this.buildElement('button', { class: 'right-chevron' }, [
+          this.buildElement('i', { class: 'fas fa-chevron-circle-right' })
+        ]),
+      ])
+    ])
+  }
+
+  handleOverlayClicks(event) {
+    if (event.target.matches('.overlay')) return;
+    this.$carousel.querySelector(`img[data-id="${this.id}"]`).classList.add('hidden')
+    if (event.target.classList.value.includes('left')) this.rotateImage('left')
+    else if (event.target.classList.value.includes('right')) this.rotateImage('right')
+    else if (event.target.classList.value.includes('dot')) this.id = Number(event.target.dataset.id)
+    this.$carousel.querySelector(`img[data-id="${this.id}"]`).classList.remove('hidden')
+    Array.from(this.$dots.children).forEach(dot => {
+      (Number(dot.dataset.id) === this.id)
+        ? dot.firstElementChild.classList.add('filled')
+        : dot.firstElementChild.classList.remove('filled')
+    })
+  }
+
+  rotateImage(direction) {
+    if (direction === 'right') {
+      (this.id === urls.length) ? this.id = 1 : this.id++
     } else {
-      id++;
-    }
-  } else {
-    if (id === 1) {
-      id = urls.length;
-    } else {
-      id--;
+      (this.id === 1) ? this.id = urls.length : this.id--
     }
   }
 }
 
-const carousel = buildCarousel(urls);
-
-function buildCarousel(images) {
-}
-
-function buildElement(tag, attr, children) {
-  const el = document.createElement(tag);
-  for (var key in attr) (key === 'textContent')
-    ? el.textContent = attr[key]
-    : key.includes('dataset')
-      ? el.dataset[key.split('-')[1]] = attr[key]
-      : el.setAttribute(key, attr[key]);
-  console.log(children)
-  if (children) children.forEach(child => (child) && el.appendChild(child));
-  return el;
-}
+const carousel = new Carousel(urls);
+const carousel2 = new Carousel(urls);
+body.appendChild(carousel.$carousel);
+body.appendChild(carousel2.$carousel);
